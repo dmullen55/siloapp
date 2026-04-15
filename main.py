45de8029db2f5fd7c9fa75a42c9a9dfc672d9e0a
dmullen55 <davidmullen55@gmail.com>
2026-04-15 15:22:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.clock import Clock
-from kivy.logger import Logger
 from kivy.uix.popup import Popup
 
-# ---------------------------
-# VERY SAFE ERROR POPUP
-# ---------------------------
+
 def show_error(message):
     def _popup(dt):
         Popup(
@@ -20,9 +18,6 @@ def show_error(message):
     Clock.schedule_once(_popup)
 
 
-# ---------------------------
-# KV (MINIMAL & VALID)
-# ---------------------------
 Builder.load_string("""
 ScreenManager:
     MainScreen:
@@ -42,21 +37,9 @@ class MainScreen(Screen):
 
 
 class SiloApp(App):
-
     def build(self):
-        Logger.info("APP: build() called")
-        sm = ScreenManager()
-        sm.add_widget(MainScreen())
-        return sm
-
-    def on_start(self):
-        Logger.info("APP: on_start() called")
-        Clock.schedule_once(self.safe_post_start, 1)
-
-    def safe_post_start(self, dt):
-        Logger.info("APP: app stable, no crash")
+        return ScreenManager()
 
 
 if __name__ == "__main__":
     SiloApp().run()
-``
